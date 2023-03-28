0f1e8775f2c99ba29c7f0da9d2d27853bc6c4e46
franmartos11 <franmartos11@gmail.com>
2023-03-28 19:38:55 -0300

    h3

--- src/components/ServiceTop.jsx
+++ src/components/ServiceTop.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ServiceTop(){
     return(
-        <div class="servicesTop">
+        <div class="contactus">
             <div>
-                <h4>Nuestro equipo de expertos en tecnología está comprometido a entregar soluciones personalizadas y eficientes que cumplan y excedan tus expectativas.</h4>
+                <h3>Nuestro equipo de expertos en tecnología está comprometido a entregar soluciones personalizadas y eficientes que cumplan y excedan tus expectativas.</h3>
             </div>
              
         </div>
