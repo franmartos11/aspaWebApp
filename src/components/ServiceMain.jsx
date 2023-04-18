13a8ab6145d524e58694457d21f5b8119a1c4d87
franmartos11 <franmartos11@gmail.com>
2023-04-18 11:07:43 -0300

    fix last button

--- src/components/ServiceMain.jsx
+++ src/components/ServiceMain.jsx
@@ -3,6 +3,9 @@ import CardMain from "./CardMain";
 import Iphone from "./Iphone";
 
 function ServiceMain(){
+    function handleClick() {
+        window.location.href = 'https://aspadevelopment.000webhostapp.com/Aspa_Limited/Aspa_Limited/Menu.pdf';
+      }
     
     return(
         <div>
@@ -12,7 +15,7 @@ function ServiceMain(){
                     <h5>APLICACIONES A TU MEDIDA</h5>
                     <Iphone></Iphone>
                     <p>En nuestra empresa ofrecemos servicios de desarrollo de aplicaciones de alta calidad para asegurarnos de que nuestros clientes obtengan el producto final que están buscando. Trabajamos estrechamente con nuestros clientes para entender sus requisitos y necesidades, y utilizamos tecnologías y herramientas de vanguardia para garantizar que nuestras aplicaciones sean de alta calidad y cumplan con las expectativas de nuestros clientes.</p>
-                    <button class="botonBlanco" onclick="location.href='/Aspa_Limited/Aspa_Limited/Menu.pdf'">SABER MAS</button>
+                    <button class="botonBlanco" onClick={handleClick()}>SABER MAS</button>
                 </div>  
             </div>
         </div>
