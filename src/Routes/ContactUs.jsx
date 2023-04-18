45696b71536e63d748c77477a22b1d48b969fc8a
franmartos11 <franmartos11@gmail.com>
2023-04-18 11:33:22 -0300

    fix

--- src/Routes/ContactUs.jsx
+++ src/Routes/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs = () =>{
     return(
         <div>
             <div class="contactus">
-                <h3>Contactenos para discutir sus necesidades y cómo podemos ayudarle. ¡Estaremos encantados de trabajar con usted y ayudarle a hacer realidad su proyecto!</h3>
+                <h3>Contactenos para discutir sus necesidades y cómo podemos ayudarle.¡Estaremos encantados de trabajar con usted y ayudarle a hacer realidad su proyecto!</h3>
             </div>
             <Form></Form> 
         </div>
