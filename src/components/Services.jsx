6e00d6bd75c7545afeaa6ec53f50e986a3ce7724
franmartos11 <franmartos11@gmail.com>
2023-08-03 11:55:08 -0300

    fix performance images, change of format

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -24,14 +24,14 @@ function Services(){
             </div>
             <div class="cards_servicios">
                 <div class="card_servicios">
-                <img src="\img\pexels-torsten-dettlaff-54284 (2).jpg" alt=""></img>
+                <img src="\img\pexels-torsten-dettlaff-54284 (2).webp" alt=""></img>
                     <h5>APLICACIONES</h5>
                     <p>Disponemos de aplicaciones desarrolladas para que implementes en tu empresa, adaptándolas a tus necesidades.</p>
                     <Link to="/servicios" className="botonBlanco" onClick={scrollToTop}>SABER MAS</Link>
                 </div>
                 
                 <div class="card_servicios">
-                    <img src="\img\pexels-george-milton-7014599 (1).jpg" alt=""></img>
+                    <img src="\img\pexels-george-milton-7014599 (1).webp" alt=""></img>
                     <h5>APLICACIONES PERSONALIZADAS</h5>
                     <p>Desarrollamos la aplicación que necesites, con los mejores tiempos de entrega.</p>
                     <Link to="/servicios" className="botonBlanco" onClick={scrollToTop}>SABER MAS</Link>
