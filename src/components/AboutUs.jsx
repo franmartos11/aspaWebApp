2ece22fe3ead4c0e42ea92ef02c5f38f7d61c07c
franmartos11 <franmartos11@gmail.com>
2023-10-24 16:08:40 -0300

    cambio about us personas empresa

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -14,16 +14,23 @@ function AboutUs() {
                     <img src="\img\1650166774232.webp" alt="Imagen_PM"></img>
                     <h5>Haik Martin Kilic Aslan</h5>
                     <p>Project Manager</p>
-                    <p>Experiencia: Responsable de desarrollo de proyectos</p>
-                    <p>(Autocity)</p>
+                    <p>Responsable de desarrollo de proyectos</p>
                 </div>
                 
                 <div class="persona">
-                    <img src="\img\1651492193397.webp" alt="Imagen_SM"></img>
-                    <h5>Pablo Agustin Gil</h5>
-                    <p>Sales Manager</p>
-                    <p>Experiencia: Administrador contable</p>
-                    <p>(P&G Consultores)</p>
+                    <img src="\img\franM.png" alt="Imagen_SM"></img>
+                    <h5>Francisco Martos</h5>
+                    <p>Arquitecto de Software</p>
+                    <p>Responsable de diseñar la estructura y la organización de sistemas de software</p>
+                </div>
+            </div>
+            <div class="personal">
+                <div class="persona">
+                    <img src="\img\agusL.jpeg" alt="Imagen_SM"></img>
+                    <h5>Agustin Luque</h5>
+                    <p>Encargado Comercial</p>
+                    <p>Responsable de la gestion comercial y adquisicion de clientes</p>
+                    
                 </div>
             </div>
             
